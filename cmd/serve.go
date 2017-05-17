de1abdbd40c6815552e9bf9d58c4293aab1beca3
Jonathan Bunde-Pedersen <jonathanbp@gmail.com>
2017-05-17 21:06:15 +0200

    Refactored the herder

--- cmd/serve.go
+++ cmd/serve.go
@@ -5,17 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io/ioutil"
-	"net"
+	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/Webstrates/golem-herder/golem"
 
-	"github.com/fsouza/go-dockerclient"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
@@ -24,49 +19,6 @@ import (
 type Emet struct {
 	Id      string
 	BaseUrl string
-}
-
-func GetPort() int {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		panic(err)
-	}
-
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		panic(err)
-	}
-	defer l.Close()
-	return l.Addr().(*net.TCPAddr).Port
-}
-
-func GetName(id string) string {
-	return fmt.Sprintf("golem-%s", id)
-}
-
-func ContainersMatching(client *docker.Client, matches func(container *docker.APIContainers) bool) ([]docker.APIContainers, error) {
-	containers, err := client.ListContainers(docker.ListContainersOptions{All: false})
-	if err != nil {
-		log.WithError(err).Error("Error listing containers")
-		return nil, err
-	}
-
-	matching := []docker.APIContainers{}
-	for _, container := range containers {
-		if matches(&container) {
-			matching = append(matching, container)
-		}
-	}
-	return matching, nil
-}
-
-func ContainerHasName(container *docker.APIContainers, name string) bool {
-	for _, containerName := range container.Names {
-		if containerName == "/"+name {
-			return true
-		}
-	}
-	return false
 }
 
 func EmetHandler(w http.ResponseWriter, r *http.Request) {
@@ -86,19 +38,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 func ListHandler(w http.ResponseWriter, r *http.Request) {
 
-	client, err := docker.NewClientFromEnv()
-	if err != nil {
-		log.WithError(err).Error("Error creating docker client")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	golems, err := ContainersMatching(client, func(container *docker.APIContainers) bool {
-		return strings.HasPrefix(container.Image, "webstrates/golem")
-	})
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-	}
+	golems, err := golem.List()
 
 	data, err := json.Marshal(golems)
 	if err != nil {
@@ -113,96 +53,13 @@ func SpawnHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	wsid := vars["id"]
 
-	client, err := docker.NewClientFromEnv()
-	if err != nil {
-		log.WithError(err).Error("Could create docker client")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	//ctx := context.Background()
-
-	repository := "webstrates/golem"
-	tag := "latest"
-
-	log.WithFields(log.Fields{"image": fmt.Sprintf("%s:%s", repository, tag)}).Info("Pulling image")
-
-	err = client.PullImage(docker.PullImageOptions{
-		Repository: "webstrates/golem",
-		Tag:        "latest",
-	}, docker.AuthConfiguration{})
+	containerID, err := golem.Spawn(wsid)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	log.WithFields(log.Fields{"image": fmt.Sprintf("%s:%s", repository, tag)}).Info("Pull done")
 
-	// Get current dir
-	dir, err := os.Getwd()
-	if err != nil {
-		log.WithError(err).Error("Could not discover current directory")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	seccomp, err := ioutil.ReadFile(filepath.Join(dir, "chrome.json"))
-	if err != nil {
-		log.WithError(err).Error("Could not read seccomp profile")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	log.WithFields(log.Fields{"webstrateid": wsid}).Info("Creating container")
-	container, err := client.CreateContainer(
-		docker.CreateContainerOptions{
-			Name: GetName(wsid),
-			Config: &docker.Config{
-				Image: "webstrates/golem:latest",
-				ExposedPorts: map[docker.Port]struct{}{
-					"9222/tcp": {},
-				},
-				Env: []string{fmt.Sprintf("WEBSTRATEID=%s", wsid)},
-				Cmd: []string{
-					"--headless",
-					"--ignore-certificate-errors",
-					"--disable-gpu",
-					"--remote-debugging-address=0.0.0.0",
-					"--remote-debugging-port=9222",
-					fmt.Sprintf("http://webstrates/%s", wsid),
-				},
-			},
-			HostConfig: &docker.HostConfig{
-				Links: []string{"webstrates"},
-				PortBindings: map[docker.Port][]docker.PortBinding{
-					"9222/tcp": []docker.PortBinding{
-						docker.PortBinding{
-							HostIP:   "0.0.0.0",
-							HostPort: fmt.Sprintf("%d", GetPort()),
-						},
-					},
-				},
-				SecurityOpt: []string{
-					fmt.Sprintf("seccomp=%s", string(seccomp)),
-				},
-			},
-		},
-	)
-	if err != nil {
-		log.WithError(err).Error("Error creating container")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-	log.WithFields(log.Fields{"webstrateid": wsid, "containerid": container.ID}).Info("Created container, starting ...")
-
-	err = client.StartContainer(container.ID, nil)
-
-	if err != nil {
-		log.WithError(err).Error("Error starting container")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	w.Write([]byte(fmt.Sprintf("%s lumbering along", container.ID)))
+	w.Write([]byte(fmt.Sprintf("%s lumbering along", containerID)))
 
 }
 func ResetHandler(w http.ResponseWriter, r *http.Request) {
@@ -216,39 +73,11 @@ func KillHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	wsid := vars["id"]
 
-	client, err := docker.NewClientFromEnv()
-	if err != nil {
-		log.WithError(err).Error("Error creating docker client")
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	golems, err := ContainersMatching(client, func(c *docker.APIContainers) bool {
-		return strings.HasPrefix(c.Image, "webstrates/golem") && ContainerHasName(c, GetName(wsid))
-	})
-
-	if len(golems) != 1 {
-		http.Error(w, fmt.Sprintf("Unexpected amount of golems - %d", len(golems)), 500)
-		return
-	}
-
-	err = client.KillContainer(docker.KillContainerOptions{
-		ID: golems[0].ID,
-	})
+	err := golem.Kill(wsid)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-
-	err = client.RemoveContainer(docker.RemoveContainerOptions{
-		ID:            golems[0].ID,
-		RemoveVolumes: true,
-	})
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
 	w.Write([]byte(fmt.Sprintf("Golem for %s is no more", wsid)))
 }
 
---
+++ golem/golem.go
@@ -0,0 +1,202 @@
+package golem
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+func getPort() int {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		panic(err)
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port
+}
+
+func getName(id string) string {
+	return fmt.Sprintf("golem-%s", id)
+}
+
+func containersMatching(client *docker.Client, matches func(container *docker.APIContainers) bool) ([]docker.APIContainers, error) {
+	containers, err := client.ListContainers(docker.ListContainersOptions{All: false})
+	if err != nil {
+		log.WithError(err).Error("Error listing containers")
+		return nil, err
+	}
+
+	matching := []docker.APIContainers{}
+	for _, container := range containers {
+		if matches(&container) {
+			matching = append(matching, container)
+		}
+	}
+	return matching, nil
+}
+
+func containerHasName(container *docker.APIContainers, name string) bool {
+	for _, containerName := range container.Names {
+		if containerName == "/"+name {
+			return true
+		}
+	}
+	return false
+}
+
+func Spawn(webstrateID string) (string, error) {
+
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		log.WithError(err).Error("Could create docker client")
+		return "", err
+	}
+
+	repository := "webstrates/golem"
+	tag := "latest"
+
+	log.WithFields(log.Fields{"image": fmt.Sprintf("%s:%s", repository, tag)}).Info("Pulling image")
+
+	err = client.PullImage(docker.PullImageOptions{
+		Repository: "webstrates/golem",
+		Tag:        "latest",
+	}, docker.AuthConfiguration{})
+	if err != nil {
+		return "", err
+	}
+	log.WithFields(log.Fields{"image": fmt.Sprintf("%s:%s", repository, tag)}).Info("Pull done")
+
+	// Get current dir
+	dir, err := os.Getwd()
+	if err != nil {
+		log.WithError(err).Error("Could not discover current directory")
+		return "", err
+	}
+
+	seccomp, err := ioutil.ReadFile(filepath.Join(dir, "chrome.json"))
+	if err != nil {
+		log.WithError(err).Error("Could not read seccomp profile")
+		return "", err
+	}
+
+	log.WithFields(log.Fields{"webstrateid": webstrateID}).Info("Creating container")
+	container, err := client.CreateContainer(
+		docker.CreateContainerOptions{
+			Name: getName(webstrateID),
+			Config: &docker.Config{
+				Image: "webstrates/golem:latest",
+				ExposedPorts: map[docker.Port]struct{}{
+					"9222/tcp": {},
+				},
+				Env: []string{fmt.Sprintf("WEBSTRATEID=%s", webstrateID)},
+				Cmd: []string{
+					"--headless",
+					"--ignore-certificate-errors",
+					"--disable-gpu",
+					"--remote-debugging-address=0.0.0.0",
+					"--remote-debugging-port=9222",
+					fmt.Sprintf("http://webstrates/%s", webstrateID),
+				},
+			},
+			HostConfig: &docker.HostConfig{
+				Links: []string{"webstrates"},
+				PortBindings: map[docker.Port][]docker.PortBinding{
+					"9222/tcp": []docker.PortBinding{
+						docker.PortBinding{
+							HostIP:   "0.0.0.0",
+							HostPort: fmt.Sprintf("%d", getPort()),
+						},
+					},
+				},
+				SecurityOpt: []string{
+					fmt.Sprintf("seccomp=%s", string(seccomp)),
+				},
+			},
+		},
+	)
+	if err != nil {
+		log.WithError(err).Error("Error creating container")
+		return "", err
+	}
+	log.WithFields(log.Fields{"webstrateid": webstrateID, "containerid": container.ID}).Info("Created container, starting ...")
+
+	err = client.StartContainer(container.ID, nil)
+
+	if err != nil {
+		log.WithError(err).Error("Error starting container")
+		return "", err
+	}
+	return container.ID, nil
+}
+
+func Kill(webstrateID string) error {
+
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		log.WithError(err).Error("Error creating docker client")
+		return err
+	}
+
+	golems, err := containersMatching(client, func(c *docker.APIContainers) bool {
+		return strings.HasPrefix(c.Image, "webstrates/golem") && containerHasName(c, getName(webstrateID))
+	})
+
+	if len(golems) != 1 {
+		return fmt.Errorf("Unexpected amount of golems - %d", len(golems))
+	}
+
+	err = client.KillContainer(docker.KillContainerOptions{
+		ID: golems[0].ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = client.RemoveContainer(docker.RemoveContainerOptions{
+		ID:            golems[0].ID,
+		RemoveVolumes: true,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Restart(webstrateID string) (string, error) {
+	err := Kill(webstrateID)
+	if err != nil {
+		return "", err
+	}
+	return Spawn(webstrateID)
+}
+
+func List() ([]docker.APIContainers, error) {
+
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		log.WithError(err).Error("Error creating docker client")
+		return nil, err
+	}
+
+	golems, err := containersMatching(client, func(container *docker.APIContainers) bool {
+		return strings.HasPrefix(container.Image, "webstrates/golem")
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return golems, nil
+
+}
