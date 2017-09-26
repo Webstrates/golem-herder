e9aafee6fe0c3eed30e5fab5769fe13e1d96dd86
Jonathan Bunde-Pedersen <jonathanbp@gmail.com>
2017-09-26 14:42:54 +0200

    Improved tag-and-release procedure

--- tagandrelease.sh
+++ tagandrelease.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git tag -a "$1" -m "$2"
+git tag -a "$1" -m "$2" && git push && git push --tags
 gox --output "out/{{.Dir}}_$1_{{.OS}}_{{.Arch}}"
 ghr -u Webstrates -r golem-herder "$1" out
