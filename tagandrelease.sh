7b205600c05fd79f025b7c2ce3ec2c557282623b
Jonathan Bunde-Pedersen <jonathanbp@gmail.com>
2018-03-22 16:48:34 +0100

    Update release script

--- tagandrelease.sh
+++ tagandrelease.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-git tag -a "$1" -m "$2" && git push && git push --tags
+git tag -a "$1" -m "$2" && git push && git push --tags || true
+rm -fr out && mkdir out
 gox --output "out/{{.Dir}}_$1_{{.OS}}_{{.Arch}}"
 ghr -u Webstrates -r golem-herder "$1" out
