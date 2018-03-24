814a86d09647e1aa7f7ffc4fd4a2f7ed6e72f1a9
Jonathan Bunde-Pedersen <jonathanbp@gmail.com>
2018-03-24 11:08:50 +0100

    Slightly better tagandrelease.sh

--- tagandrelease.sh
+++ tagandrelease.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-git tag -a "$1" -m "$2" && git push && git push --tags || true
+git co master && git merge develop && git tag -a "$1" -m "$2" && git push && git push --tags && git co develop || true
 rm -fr out && mkdir out
 gox --output "out/{{.Dir}}_$1_{{.OS}}_{{.Arch}}"
 ghr -u Webstrates -r golem-herder "$1" out
